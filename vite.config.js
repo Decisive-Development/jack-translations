55bf317b1f7cb3820505e6c436e3d61495b5df29
Decisive Development <decisive.development1@gmail.com>
2025-01-12 19:49:50 +0000

    removed vite build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		sitemap({
-			hostname: 'https://chamberlainuebersetzung.com',
-			routes: [
-				{ path: '/', lastmod: new Date().toISOString() },
-			],
-		})
-	]
+	plugins: [sveltekit()]
 });
